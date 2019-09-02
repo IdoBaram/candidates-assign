6e4ebadea2212942187566c885c023389bcb5544
= <idobaram1990@gmail.com>
2019-09-02 18:28:07 +0300

    Commiting the code after completing the project

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo cat /vagrant/id_rsa2 >> /home/vagrant/.ssh/id_rsa &&
+sudo cat /vagrant/id_rsa2.pub >> /home/vagrant/.ssh/id_rsa.pub &&
+sudo cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys &&
+sudo chmod 700 /home/vagrant/.ssh &&
+sudo chmod 600 /home/vagrant/.ssh/authorized_keys &&
+sudo printf "StrictHostKeyChecking no\nUserKnownHostsFile=/dev/null" >> /home/vagrant/.ssh/config &&
+sudo service ssh restart &&
+sudo rm -r /vagrant/id*
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+if (( $# < 2 )); then
+    echo "Please insert a minum of 2 arguments"
+    exit 1
+fi
+
+destination=${@: -1}
+files=${@/$destination/""}
+all_file_size=0
+if [ "$(uname -n)" = "server1" ];
+then
+	destServer=server2
+	echo $destServer
+else
+	destServer=server1
+	echo $destServer
+fi
+
+for var in $files
+	do
+		filesize=$(stat -c%s "$var")
+		all_file_size=$(echo "$all_file_size+$filesize" | bc)
+		scp $var $destServer:$destination
+	done
+echo "$all_file_size";
